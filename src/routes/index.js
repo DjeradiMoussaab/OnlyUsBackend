fff7653d6cb2726aaf6f7bbb940fc318328a7c5a
Moussaab Djeradi <fm_djeradi@esi.dz>
2026-01-26 19:50:14 +0100

    remove uncessary code

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import userRoutes from "./user.routes.js"
 
 const router = Router()
 
-router.get("/health", (req, res) => res.json({ ok: true }))
 router.use("/auth", authRoutes)
 router.use("/users", userRoutes)
 
